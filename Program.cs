2e9c53392e7ae21ca082bc8963272cb1a5220f93
huynq1992hpvn <133644743+huynq1992hpvn@users.noreply.github.com>
2023-05-29 17:26:28 +0700

    lan1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiemtrasonguyento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int songuyento;
+            Console.WriteLine("moi ban nhap so ");
+            songuyento= int.Parse(Console.ReadLine()); 
+            if (songuyento < 2)
+            {
+                Console.WriteLine("day khong phai so nguyen to");
+            }
+            else 
+            {
+                int i = 2;
+                bool kiemtra = false;
+                while (i <= Math.Sqrt(songuyento))
+                {
+                    if (songuyento % i == 0)
+                    {
+                        kiemtra = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (kiemtra == true)
+                {
+                    Console.WriteLine("day la so nguyen to");
+                }
+                else
+                {
+                    Console.WriteLine("day khong phai so nguyen to");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
